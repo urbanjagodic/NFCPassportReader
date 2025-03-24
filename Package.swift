2aa1450a69165f2b9dba35d8d7f23ec400d4397a
urbanjagodic <urban.jagodic@rekono.si>
2025-03-24 12:55:42 +0100

    changed OpenSSL-package back to OpenSSL to vresion 3.3.2000

--- Package.swift
+++ Package.swift
@@ -16,16 +16,14 @@ let package = Package(
             targets: ["NFCPassportReader"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/krzyzanowskim/OpenSSL-Package.git", from: "3.3.1000")
+        .package(url: "https://github.com/krzyzanowskim/OpenSSL.git", from: "3.3.2000")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "NFCPassportReader",
-            dependencies: [
-                .product(name: "OpenSSL", package: "OpenSSL-Package")
-            ]),
+            dependencies: ["OpenSSL"]),
     ]
 )
 
